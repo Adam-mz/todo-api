bbeee09b818812afb3e807ae1409c73061b20767
Adam-mz <vishanov.ad@yandex.ru>
2025-04-17 08:01:08 +0300

    Add GET and POST handlers for task

--- main.go
+++ main.go
@@ -1,8 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
+
+type requestBody struct {
+	Task string `json:"task"`
+}
+
+var task string
+
+func taskGetHandler(c echo.Context) error {
+	return c.JSON(http.StatusOK, "Hello, "+task)
+}
+
+func taskPostHandler(c echo.Context) error {
+	var body requestBody
+	if err := c.Bind(&body); err != nil {
+		return c.JSON(http.StatusBadRequest, "Invalid request body")
+	}
+	task = body.Task
+	return c.JSON(http.StatusOK, "Task created")
+}
 
 func main() {
-	// Initialize the application
-	fmt.Println("Hello, World!")
+	e := echo.New()
+
+	e.POST("/task", taskPostHandler)
+	e.GET("/task", taskGetHandler)
+
+	e.Start(":8080")
+
 }
